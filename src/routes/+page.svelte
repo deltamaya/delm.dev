eb169e40c446c4cc3007b21fa03a8bad59028d34
deltamaya <deltamaya553@gmail.com>
2024-12-09 00:29:22 +0800

    about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,81 +1,90 @@
-<script>
-    import {fly} from "svelte/transition";
+<script lang="ts">
+    import { fly } from "svelte/transition";
     import IconButton from "./IconButton.svelte";
     import MenuItem from "./MenuItem.svelte";
-    import {socialLinks, menuItems, techStacks} from "$lib/data";
-
+    import { socialLinks, menuItems } from "$lib/data";
 </script>
 
 <div
-        class="min-h-[calc(100vh-2.5rem)] flex justify-between items-center px-32 text-center overflow-hidden
+    class="min-h-[calc(100vh-2.5rem)] flex justify-between items-center px-32 text-center overflow-hidden
     -z-10
 "
 >
     <div
-            class="w-1/2 flex flex-col justify-between items-center z-10
+        class="w-1/2 flex flex-col justify-between items-center z-10
     "
     >
         <div class="flex items-center title-float">
             <img
-                    src="/avatar.jpg"
-                    alt="avatar"
-                    class="w-32 rounded-full border-4 drop-shadow-2xl"
-                    in:fly={{ y: -50, duration: 400 }}
+                src="/avatar.jpg"
+                alt="avatar"
+                class="w-32 rounded-full border-4 drop-shadow-2xl"
+                in:fly={{ y: -50, duration: 400 }}
             />
             <div class="flex flex-col ml-5">
                 <p
-                        class="text-8xl font-bold text-gray-800 mt-2"
-                        in:fly={{ y: -50, duration: 400, delay: 50 }}
+                    class="text-8xl font-bold text-gray-800 mt-2"
+                    in:fly={{ y: -50, duration: 400, delay: 50 }}
                 >
                     delm.<span class="text-red-500">dev</span>
                 </p>
                 <div
-                        class="flex justify-center space-x-4 mt-1 items-center"
+                    class="flex justify-center space-x-4 mt-1 items-center"
                 ></div>
             </div>
         </div>
         <div class="flex flex-col items-center mt-16">
             <h1
-                    class="text-4xl md:text-6xl font-bold text-gray-800 mb-4"
-                    in:fly={{ y: 50, duration: 400, delay: 50 }}
+                class="text-4xl md:text-6xl font-bold text-gray-800 mb-4"
+                in:fly={{ y: 50, duration: 400, delay: 50 }}
             >
                 Hi, I'm <span class="text-red-500">Maya☆</span>.
             </h1>
             <p
-                    class="text-lg md:text-xl text-gray-600 max-w-2xl mb-8"
-                    in:fly={{ y: 50, duration: 400, delay: 100 }}
+                class="text-lg md:text-xl text-gray-600 max-w-2xl mb-8"
+                in:fly={{ y: 50, duration: 400, delay: 100 }}
             >
                 Problem Solver. Life Enthusiast. Work Hard, Play Hard.
             </p>
-
-
         </div>
         <div class="flex space-x-4 justify-center">
             {#each socialLinks as item, index (index)}
                 <div
-                        in:fly|global={{ y: 50, duration: 400, delay: 150 +25* index }}
+                    in:fly|global={{
+                        y: 50,
+                        duration: 400,
+                        delay: 150 + 25 * index,
+                    }}
                 >
-                    <IconButton href={item.href} label={item.label} iconClass={item.iconClass}/>
+                    <IconButton
+                        href={item.href}
+                        label={item.label}
+                        iconClass={item.iconClass}
+                    />
                 </div>
             {/each}
         </div>
-
     </div>
     <div class="w-1/2 items-center flex flex-col z-10 space-y-5 justify-center">
         {#each menuItems as item, index (index)}
             <div
-                    class="w-full ml-48"
-                    in:fly|global={{ x: 100, duration: 400, delay: 50*index }}
+                class="w-full ml-48"
+                in:fly|global={{ x: 100, duration: 400, delay: 50 * index }}
             >
-                <MenuItem href={item.href} label={item.label} title={item.title} iconClass={item.iconClass}/>
+                <MenuItem
+                    href={item.href}
+                    label={item.label}
+                    title={item.title}
+                    iconClass={item.iconClass}
+                />
             </div>
         {/each}
     </div>
 
     <img
-            src="/logo.png"
-            alt="logo"
-            class="blurred_background"
-            in:fly={{ y: 100, duration: 400 }}
+        src="/logo.png"
+        alt="logo"
+        class="blurred_background"
+        in:fly={{ y: 100, duration: 400 }}
     />
 </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,52 +1,90 @@
-<script>
-    import {fly} from "svelte/transition";
+<script lang="ts">
+    import { fly } from "svelte/transition";
     import IconButton from "../IconButton.svelte";
-    import {techStacks} from "$lib/data.ts";
-
+    import { languages, operatingSystems } from "$lib/data.ts";
 </script>
+
 <div class="min-h-[calc(100vh-2.5rem)] flex justify-center items-center">
     <div class="flex flex-col items-center">
         <img
-                src="/avatar.jpg"
-                alt="avatar"
-                class="w-48 rounded-full border-4 drop-shadow-2xl"
-                in:fly={{ y: -50, duration: 400 }}
+            src="/avatar.jpg"
+            alt="avatar"
+            class="w-48 rounded-full border-4 drop-shadow-2xl"
+            in:fly={{ y: -50, duration: 400 }}
         />
         <div class="flex flex-col ml-5 items-center">
             <p
-                    class="text-8xl font-bold text-gray-800 mt-2"
-                    in:fly={{ y: -50, duration: 400, delay: 50 }}
+                class="text-8xl font-bold text-gray-800 mt-2"
+                in:fly={{ y: -50, duration: 400, delay: 50 }}
             >
                 delta<span class="text-red-500">maya</span>
             </p>
             <div class="text-gray-400 text-xl font-bold mt-5">
                 Wuhan University of Technology
             </div>
-                <div><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>.NET</title><path d="M24 8.77h-2.468v7.565h-1.425V8.77h-2.462V7.53H24zm-6.852 7.565h-4.821V7.53h4.63v1.24h-3.205v2.494h2.953v1.234h-2.953v2.604h3.396zm-6.708 0H8.882L4.78 9.863a2.896 2.896 0 0 1-.258-.51h-.036c.032.189.048.592.048 1.21v5.772H3.157V7.53h1.659l3.965 6.32c.167.261.275.442.323.54h.024c-.04-.233-.06-.629-.06-1.185V7.529h1.372zm-8.703-.693a.868.829 0 0 1-.869.829.868.829 0 0 1-.868-.83.868.829 0 0 1 .868-.828.868.829 0 0 1 .869.829Z"/></svg></div>
 
-            <div class="text-gray-800 text-xl font-bold">
+            <div
+                class="text-gray-800 text-xl font-bold justify-center items-center"
+            >
                 Student
             </div>
-            <div class="flex font-bold text-gray-800 text-2xl">
-  <div class="flex space-x-4 justify-center mt-5 ">
-            {#each techStacks as item, index (index)}
-                <div
-                        in:fly|global={{ y: 50, duration: 400, delay: 150 +25* index }}
-                >
-                    <IconButton href={item.href} label={item.label} iconClass={item.iconClass}/>
+            <div
+                class="flex font-bold text-gray-800 text-2xl justify-center items-center"
+            >
+                <div class="items-center mt-4 mr-1">
+                    maya.lang = std::vector&lbrace;
                 </div>
-            {/each}
-        </div>
+                <div class="flex space-x-4 justify-center mt-5">
+                    {#each languages as item, index (index)}
+                        <div
+                            in:fly|global={{
+                                y: 50,
+                                duration: 400,
+                                delay: 100 + 25 * index,
+                            }}
+                        >
+                            <IconButton
+                                href={item.href}
+                                label={item.label}
+                                iconClass={item.iconClass}
+                            />
+                        </div>
+                    {/each}
+                </div>
+                <div class="mt-4 ml-1">&rbrace;</div>
             </div>
-
-
+            <div
+                class="flex font-bold text-gray-800 text-2xl justify-center items-center"
+            >
+                <div class="items-center mt-4 mr-1">
+                    maya.os = std::set&lbrace;
+                </div>
+                <div class="flex space-x-4 justify-center mt-4">
+                    {#each operatingSystems as item, index (index)}
+                        <div
+                            in:fly|global={{
+                                y: 50,
+                                duration: 400,
+                                delay: 100 + 25 * index,
+                            }}
+                        >
+                            <IconButton
+                                href={item.href}
+                                label={item.label}
+                                iconClass={item.iconClass}
+                            />
+                        </div>
+                    {/each}
+                </div>
+                <div class="mt-4 ml-1">&rbrace;</div>
+            </div>
+        </div>
     </div>
-</div>
 </div>
 
 <img
-        src="/about_bg.jpg"
-        alt="logo"
-        class="blurred_background"
-        in:fly={{ y: 50, duration: 400 }}
+    src="/about_bg.jpg"
+    alt="logo"
+    class="blurred_background"
+    in:fly={{ y: 50, duration: 400 }}
 />
