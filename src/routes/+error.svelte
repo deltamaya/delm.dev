6f92a328a1c470ebb3f4e20a3d01f397934b6493
deltamaya <deltamaya553@gmail.com>
2024-12-12 00:04:44 +0800

    404 page mobile support

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,37 +1,36 @@
 <script>
-
 </script>
+
 <div
-        class="min-h-[calc(100vh-2.5rem)] pb-56 flex justify-center items-center text-8xl font-bold -z-10 overflow-hidden">
-    <div
-            class="z-10">
-        <img src="/not_found_display.png" alt="not found"
-             class="h-80 rounded-2xl border-4 border-white mb-5 drop-shadow-2xl select-none pointer-events-none"/>
+    class="min-h-[calc(100vh-2.5rem)] pb-56 flex justify-center items-center lg:text-8xl md:text-6xl sm:text-4xl text-2xl font-bold -z-10 overflow-hidden"
+>
+    <div class="z-10">
+        <img
+            src="/not_found_display.png"
+            alt="not found"
+            class="lg:h-80 md:h-60 sm:h-50 h-40 rounded-2xl border-4 border-white mb-5 drop-shadow-2xl select-none pointer-events-none"
+        />
         <div class="flex">
             <div>
-                <div class="text-9xl">
-                    Oops!
-                </div>
+                <div class="lg:text-9xl md:text-8xl text-6xl">Oops!</div>
                 <div class="h-2 w-full bg-red-500"></div>
             </div>
 
-            <div class="flex flex-col ml-10  pl-10">
-                <div>
-                    404
-                </div>
-                <div>
-                    Not Found
-                </div>
-        <button class="text-2xl bg-red-500 border-red-500 border-2 text-gray-200 p-1
+            <div class="flex flex-col ml-10 pl-10">
+                <div>404</div>
+                <div>Not Found</div>
+                <button
+                    class="lg:text-2xl md:text-xl sm:text-lg text-sm bg-red-500 border-red-500 border-2 text-gray-200 p-1
 hover:bg-transparent hover:text-gray-800 transition-colors duration-300"
-                onclick={()=>history.back()}
-        >Let's Get the Hell Out of Here!
-        </button>
+                    onclick={() => history.back()}
+                    >Let's Get the Hell Out of Here!
+                </button>
             </div>
         </div>
-
-
     </div>
-    <img src="/not_found_display.png" alt="not found"
-         class="absolute inset-0  blurred_background w-full"/>
+    <img
+        src="/not_found_display.png"
+        alt="not found"
+        class="absolute inset-0 blurred_background w-full"
+    />
 </div>
