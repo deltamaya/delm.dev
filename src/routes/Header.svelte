edf6546ccace30e00491e9698af5c7ad1cc89eb7
deltamaya <me@delm.dev>
2025-04-25 08:25:52 +0800

    update logo

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,25 @@
 <div class="py-2 lg:px-48 md:px-32 px-8 self-center w-full flex lg:text-4xl md:text-3xl sm:text-2xl text-xl sticky top-0 bg-neutral-900 border-b border-neutral-600 bg-opacity-70 backdrop-blur-xl">
     <a href="https://delm.dev">
-        <img src="logo-white.png" class="lg:w-[60px] lg:h-[35px] md:w-[50px] md:h-[30px] w-[50px] h-[28px] mr-4" style="opacity: 1;" alt="logo"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 3000 3000">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: #a0001b;
+                    }
+
+                    .cls-1, .cls-2 {
+                        fill-rule: evenodd;
+                    }
+
+                    .cls-2 {
+                        fill: #f1f1f1;
+                    }
+                </style>
+            </defs>
+            <path id="Triangle_1" data-name="Triangle 1" class="cls-1" d="M1858.92,1570.59l428.92,777.28H1430Z"/>
+            <path class="cls-2" d="M0,2348.16l564.317-1.38L1424.67,789.838H859.031"/>
+            <path class="cls-2" d="M712.335,2348.16l564.315-1.38L2137,789.838H1571.37"/>
+            <path class="cls-2" d="M3000,2348.16l-564.32-1.38L1575.33,789.838h565.64"/>
+        </svg>
     </a>
 </div>
