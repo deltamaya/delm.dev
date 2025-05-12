80bc8521c7cb440ffef3b03c5318370ddea44b73
deltamaya <me@delm.dev>
2025-05-12 11:10:42 +0800

    update

--- src/routes/SocialLink.svelte
+++ src/routes/SocialLink.svelte
@@ -2,14 +2,14 @@
   import {Spring} from "svelte/motion";
   import Icon from "@iconify/svelte";
 
-  let scale=new Spring(1,{stiffness:0.1,damping:0.3});
+  let scale=new Spring(1,{stiffness:0.1,damping:0.4});
   let {href,label,iconClass} =$props()
 </script>
 
 
 <a
       href={href}
-      class="lg:text-3xl md:text-2xl text-xl hover:text-red-700 transition-color duration-200 inline-block"
+      class="lg:text-3xl md:text-2xl text-xl hover:text-red-600 transition-color inline-block"
       aria-label={label}
       style="transform: scale({scale.current});"
       onmouseenter={() => scale.set(1.25)}
