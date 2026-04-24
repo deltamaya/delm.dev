4a346d85f7bd5e7534a466cc2084564c0f1be626
deltamaya <me@delm.dev>
2026-04-24 17:22:59 +0800

    fix compile error

--- src/components/ProjectsDisplay.tsx
+++ src/components/ProjectsDisplay.tsx
@@ -3,7 +3,7 @@ import { projects } from '../data'
 export default function ProjectsDisplay() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 z-[20]">
-            {projects.map(({ href, title, icon, techs, desc }, index) => (
+            {projects.map(({ href, title, icon, desc }, index) => (
                 <a
                     key={index}
                     href={href}
